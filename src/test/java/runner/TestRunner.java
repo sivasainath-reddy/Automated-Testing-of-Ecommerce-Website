a5adff865c819dc6654937a4eba0c37bcb501ad5
b64user12 <b64user12@rpsconsulting.in>
2025-03-22 10:09:18 +0530

    initial commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,18 +8,18 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = 
 {
-//        "src/test/resources/features/login",
-//        "src/test/resources/features/functionalities",
+        "src/test/resources/features/login",
+        "src/test/resources/features/functionalities",
     	"src/test/resources/features/order",
-//        "src/test/resources/features/cart",
-//        "src/test/resources/features/purchase"
+        "src/test/resources/features/cart",
+        "src/test/resources/features/purchase"
     },
     glue = {
-//    		"stepDefinitions.login",
-//    		"stepDefinitions.functional",
+    		"stepDefinitions.login",
+    		"stepDefinitions.functional",
     		"stepDefinitions.order",
-//    		"stepDefinitions.cart",
-//    		"stepDefinitions.purchase",
+    		"stepDefinitions.cart",
+    		"stepDefinitions.purchase",
     		},
     		plugin = {"pretty", "json:target/cucumber-reports-json/Cucumber.json"}, // Generate JSON report
 //    plugin = {"pretty", "html:target/cucumber-reports.html"},
